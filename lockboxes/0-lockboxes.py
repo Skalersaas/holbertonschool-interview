8d413ac96af4dc2309b22b3d1366ecc9cd85f27a
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-02-27 12:37:39 +0400

    Update 0-lockboxes.py

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -2,11 +2,19 @@
 # Lockboxes
 if __name__ == "__main__":
     def canUnlockAll(boxes):
+        #First box is always open
         opened = [0]
         for i in opened:
-            box = boxes[i]
-            opened += list(set(box)-set(opened))
+
+            # Concating keys
+            opened += list(set(boxes[i])-set(opened))
+
+            # Removing keys with no boxes
+            opened = [i for i in opened if i <= len(boxes)]
+
+            #If we got all boxes opened return true
             if len(opened)==len(boxes):
                 return True
             
+        #Otherwise false
         return False    
