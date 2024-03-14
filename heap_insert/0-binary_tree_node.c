ff368a3248a9286b01e5b173f1ddea31d7821257
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-03-14 14:08:49 +0400

    Update 0-binary_tree_node.c

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -1,12 +1,18 @@
 #include <stdlib.h>
 #include "binary_trees.h"
-binary_tree_t* binary_tree_node(binary_tree_t* parent, int value) {
-	binary_tree_t* node;
+/**
+ * struct binary_tree_node - new node
+ *
+ * @parent: pointer to the parent node
+ * @value: value of the new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+	binary_tree_t *node;
 	node = malloc(sizeof(binary_tree_t));
 	if (!node)
-		return NULL;
+		return (NULL);
 	node->parent = parent;
 	node->n = value;
 	node->left = node->right = NULL;
-	return node;
+	return (node);
 }
