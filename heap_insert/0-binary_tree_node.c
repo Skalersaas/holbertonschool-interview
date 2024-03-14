3de724c1ecfc48625f9f37f32aa97f09525fdd46
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-03-14 14:12:57 +0400

    Update 0-binary_tree_node.c

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -6,6 +6,8 @@
  *
  * @parent: pointer to the parent node
  * @value: value of the new node
+ * 
+ * Return: pointer to the new node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
