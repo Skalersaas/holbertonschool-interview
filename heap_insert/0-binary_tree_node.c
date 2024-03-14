445bb774ae0f5c3babd473ff0159c779a2eb0c22
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-03-14 14:06:29 +0400

    Update 0-binary_tree_node.c

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -2,7 +2,7 @@
 #include "binary_trees.h"
 binary_tree_t* binary_tree_node(binary_tree_t* parent, int value) {
 	binary_tree_t* node;
-	node = (struct binary_tree_t*)malloc(sizeof(binary_tree_t));
+	node = malloc(sizeof(binary_tree_t));
 	if (!node)
 		return NULL;
 	node->parent = parent;
