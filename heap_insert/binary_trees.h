da38ef2c5cfdf22ba07fc46404210d60add3a12d
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-03-15 19:11:10 +0400

    Update binary_trees.h

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -19,8 +19,10 @@ typedef struct binary_tree_s
 	struct binary_tree_s* right;
 } binary_tree_t;
 typedef struct binary_tree_s heap_t;
-void binary_tree_print(const binary_tree_t*);
-binary_tree_t *heap_insert(binary_tree_t *root, int value);
-binary_tree_t* binary_tree_node(binary_tree_t* parent, int value);
-
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+heap_t *heap_insert(heap_t **root, int value);
+void heapify_up(heap_t *node);
+size_t binary_tree_height(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
 #endif /* _BINARY_TREES_H_ */
