2336cc3d6ea5016d63f86979a329709a8a9cd4af
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-03-25 01:23:02 +0400

    Update sandpiles.h

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -3,8 +3,8 @@
 
 #include <stddef.h>
 
-static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
-static void print_grid(int grid[3][3]);
+
+
 
 int stable(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
