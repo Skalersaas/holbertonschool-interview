1507e9dc0117931dc28b50775cf8ba02bf60eb03
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-03-25 01:24:12 +0400

    Update 0-sandpiles.c

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -19,6 +19,26 @@ int stable(int grid[3][3])
 
 	return 1;
 }
+/**
+ * print_grid - Print 3x3 grid
+ * @grid: 3x3 grid
+ *
+ */
+static void print_grid(int grid[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
+}
 
 /**
  * stable - Checks if sandpile is stable
