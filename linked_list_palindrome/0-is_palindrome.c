557fb81e145e4fd499eca9694f0e9f027ec03570
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-01 21:04:14 +0400

    Create 0-is_palindrome.c

---
+++ linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+size_t list_size(const listint_t* h)
+{
+    const listint_t* current;
+    unsigned int n;
+
+    current = h;
+    n = 0;
+    while (current != NULL)
+    {
+        current = current->next;
+        n++;
+    }
+
+    return (n);
+}
+
+listint_t* get(listint_t* head, int num)
+{
+    const listint_t* current;
+
+    current = head;
+    while (current != NULL && num != 0)
+    {
+        current = current->next;
+        num--;
+    }
+    return current;
+}
+int is_palindrome(listint_t** head)
+{
+    size_t size = list_size(*head) / 2;
+    listint_t* center = get(*head, size);
+
+    int i;
+    int even;
+
+    even = size % 2;;
+    for (i = 0; i < size;i++)
+        if (get(*head, size - i - even)->n != get(center, i)->n)
+            return 0;
+    return 1;
+}
