67957b63ff85ca1a5f3fca0a3a1d932fbb48a8b7
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-08 02:31:20 +0400

    Update 0-stats.py

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-'''
-Reads stdin line by line and computes metrics
-'''
+"""Reads stdin line by line and computes metrics
+If count of lines is evenly divided by 10 and/or
+keyboardinterrupt
+all info will be printed"""
+
 import sys
 
 if __name__ == "__main__":
