936cec3a384b627b502e49fdf0285281e9b20a87
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-04-08 02:15:53 +0400

    Update 0-stats.py

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -3,36 +3,35 @@
 
 import sys
 
-file_size: int = 0
-ids = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0}
-count = 0
-
-
-def parse(line):
-    '''Parses input line and returns last two values'''
-    nums = line.rstrip().split(' ')
-    try:
-        return [int(nums[7]), int(nums[8])]
-    finally:
-        return None
-
-
-def write_info():
-    '''Writes info to the terminal'''
-    print("File size:", file_size)
-    for key in ids.keys():
-        if ids[key] > 0:
-            print(f"{key}: {ids[key]}")
-
 if __name__=="__main__":
+    file_size: int = 0
+    ids = {
+        200: 0,
+        301: 0,
+        400: 0,
+        401: 0,
+        403: 0,
+        404: 0,
+        405: 0,
+        500: 0}
+    count = 0
+
+
+    def parse(line):
+        '''Parses input line and returns last two values'''
+        nums = line.rstrip().split(' ')
+        try:
+            return [int(nums[7]), int(nums[8])]
+        finally:
+            return None
+
+
+    def write_info():
+        '''Writes info to the terminal'''
+        print("File size:", file_size)
+        for key in ids.keys():
+            if ids[key] > 0:
+                print(f"{key}: {ids[key]}")
     while True:
         try:
             for line in sys.stdin:
