ea51e1fc6a9d2c48fb693d9ff650ab157027c9c3
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-05-01 14:50:33 +0400

    Update 0-validate_utf8.py

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,32 +1,9 @@
 #!/usr/bin/python3
-"""
-method that determines if a given data set represents a valid UTF-8 encoding
-"""
-from typing import List
+"""UTF-8 validator"""
 
 
-
-def validUTF8(data: List[int]) -> bool:
-    """valid utf8"""
-    while data:
-        li = f'{data[0]:08b}'
-        if li[:2] == '10':
-            return False
-        data.pop(0)
-        count = -1
-        for i in li:
-            if i=='1':
-                count+=1
-            else:
-                break
-        if count>=4:
-            return False
-        if count==-1:
-            continue
-
-        while count!=0:
-            count-=1
-            if not data or f'{data[0]:08b}'[:2] != '10':
-                return False
-            data.pop(0)
+def validUTF8(data) -> bool:
+    """Function"""
+    if sum(item > 255 for item in data) > 0:
+        return False
     return True
