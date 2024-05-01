16688cae259f613daab7b8ffdcf8bef75afc2dce
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-05-01 13:59:08 +0400

    Update 0-validate_utf8.py

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python3
-"""UTF-8 validator"""
-
-
+def CC(item):
+    count = -1
+    for i in item:
+        if i=='1':
+            count+=1
+        else:
+            break
+    return count
 def validUTF8(data) -> bool:
-    """Function"""
-    if sum(item > 255 for item in data) > 0:
-        return False
+    while data:
+        li = f'{data[0]:08b}'
+        if li[:2] == '10':
+            return False
+        data.pop(0)
+        count = CC(li)
+        if count>=4:
+            return False
+        if count==-1:
+            continue
+
+        while count!=0:
+            count-=1
+            if not data or f'{data[0]:08b}'[:2] != '10':
+                return False
+            data.pop(0)
     return True
