23fa1b01bc41ac942ff7f9836c1f08e080607c67
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-05-01 14:51:20 +0400

    Update 0-validate_utf8.py

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -4,6 +4,25 @@
 
 def validUTF8(data) -> bool:
     """Function"""
-    if sum(item > 255 for item in data) > 0:
-        return False
+    while data:
+        li = f'{data[0]:08b}'
+        if li[:2] == '10':
+            return False
+        data.pop(0)
+        count = -1
+        for i in li:
+            if i=='1':
+                count+=1
+            else:
+                break
+        if count>=4:
+            return False
+        if count==-1:
+            continue
+
+        while count!=0:
+            count-=1
+            if not data or f'{data[0]:08b}'[:2] != '10':
+                return False
+            data.pop(0)
     return True
