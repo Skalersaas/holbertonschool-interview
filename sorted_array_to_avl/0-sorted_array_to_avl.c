5f42f816c389ec4a0559c084a4d39d9740b4746a
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-06-07 21:04:08 +0400

    Update 0-sorted_array_to_avl.c

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,99 +1,126 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 #include "binary_trees.h"
 
-/* Original code from http://stackoverflow.com/a/13755911/5184480 */
-
-/**
- * print_t - Stores recursively each level in an array of strings
- *
- * @tree: Pointer to the node to print
- * @offset: Offset to print
- * @depth: Depth of the node
- * @s: Buffer
- *
- * Return: length of printed tree after process
- */
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s)
+/*
+avl_t *create_node(int value)
 {
-	char b[6];
-	int width, left, right, is_left, i;
-
-	if (!tree)
-		return (0);
-	is_left = (tree->parent && tree->parent->left == tree);
-	width = sprintf(b, "(%03d)", tree->n);
-	left = print_t(tree->left, offset, depth + 1, s);
-	right = print_t(tree->right, offset + left + width, depth + 1, s);
-	for (i = 0; i < width; i++)
-		s[depth][offset + left + i] = b[i];
-	if (depth && is_left)
+	avl_t *node;
+	
+	node = malloc(sizeof(avl_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = value;
+	if (!node->n)
 	{
-		for (i = 0; i < width + right; i++)
-			s[depth - 1][offset + left + width / 2 + i] = '-';
-		s[depth - 1][offset + left + width / 2] = '.';
+		free(node);
+		return (NULL);
 	}
-	else if (depth && !is_left)
-	{
-		for (i = 0; i < left + width; i++)
-			s[depth - 1][offset - width / 2 + i] = '-';
-		s[depth - 1][offset + left + width / 2] = '.';
-	}
-	return (left + width + right);
+	return (node); 
 }
-
-/**
- * _height - Measures the height of a binary tree
- *
- * @tree: Pointer to the node to measures the height
- *
- * Return: The height of the tree starting at @node
- */
-static size_t _height(const binary_tree_t *tree)
+int find_middle_value(int *array, size_t size)
 {
-	size_t height_l;
-	size_t height_r;
+	size_t i;
+       	int  max, middle;
 
-	height_l = tree->left ? 1 + _height(tree->left) : 0;
-	height_r = tree->right ? 1 + _height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
-}
-
-/**
- * binary_tree_print - Prints a binary tree
- *
- * @tree: Pointer to the root node of the tree to print
- */
-void binary_tree_print(const binary_tree_t *tree)
-{
-	char **s;
-	size_t height, i, j;
-
-	if (!tree)
-		return;
-	height = _height(tree);
-	s = malloc(sizeof(*s) * (height + 1));
-	if (!s)
-		return;
-	for (i = 0; i < height + 1; i++)
+	max = array[0];
+	middle = 0;
+	// printf("finding....\n");
+	for (i = 0; i < size; ++i)
 	{
-		s[i] = malloc(sizeof(**s) * 255);
-		if (!s[i])
-			return;
-		memset(s[i], 32, 255);
-	}
-	print_t(tree, 0, 0, s);
-	for (i = 0; i < height + 1; i++)
-	{
-		for (j = 254; j > 1; --j)
+		if (max < array[i])
 		{
-			if (s[i][j] != ' ')
-				break;
-			s[i][j] = '\0';
+			max = array[i];
 		}
-		printf("%s\n", s[i]);
-		free(s[i]);
+        }
+	for (i = 0; i < size; ++i)
+	{
+		if ((max / 2)  >= array[i])
+		{
+			middle = array[i];
+		}
+        }
+	return (middle);
+}
+avl_t *add_node(avl_t *node, avl_t *node2){	
+	if(node->n > node2->n)
+		node->right = node2;
+	else
+		node->left = node2;
+	return (node);
+}
+
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	if (!array)
+		return NULL;
+
+	if (!size)
+		return NULL;
+	
+	size_t i;
+	int middle;
+	avl_t *node, *node2;
+
+	middle = find_middle_value(array, size);
+	node = create_node(middle);
+
+	for (i = 0; i < size; ++i)
+	{
+		// printf("find: (%03d)", array[i]);
+		if (middle != array[i])
+		{
+			node2 = create_node(array[i]);
+			node = add_node(node, node2);
+		}
 	}
-	free(s);
+		
+	// printf("\n");
+	// printf("\n");
+	// printf("middle %i\n", middle);
+	// printf("tree: \n");
+	// binary_tree_print(node);
+
+	return (node);
+}
+*/
+
+binary_tree_t *add_node(int *array, size_t size, avl_t *parent)
+{
+	binary_tree_t *node;
+
+	node = malloc(sizeof(avl_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->n = array[(size - 1) / 2];
+	node->right = NULL;
+	node->left = NULL;
+	node->parent = parent;
+	
+	if (size > 1)
+	{
+		node->right = add_node(array + (size + 1) / 2, size / 2, node);
+		if (node->right == NULL)
+			return (NULL);
+	}
+	if (size > 2)
+	{
+		node->left = add_node(array, (size - 1) / 2, node);
+		if (node->right == NULL)
+			return (NULL);
+	}
+
+	return (node);
+}
+
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	if (!array)
+		return NULL;
+
+	if (!size)
+		return NULL;
+		
+	return ((avl_t *) add_node(array, size, NULL));
 }
